7b63e1fdc9fddef9371f91c37e506de36d4a83c8
Ismail Ahmad <ismailahmad0505@gmail.com>
2025-12-16 02:01:18 +0500

    admin change

--- app/(tabs)/admin.tsx
+++ app/(tabs)/admin.tsx
@@ -1,21 +1,12 @@
 import Heading from '@/components/heading';
 import MainWrapper from '@/components/main_wrapper';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 
 export default function Admin(){
     return(
         <MainWrapper style={{justifyContent: 'center', alignItems: 'center', gap: 20}}>
             <Heading text='Admin' />
-            <View>
-                <Text style={{color: 'white'}}>1</Text>
-            </View>
-            <View>
-                <Text style={{color: 'white'}}>1</Text>
-            </View>
-            <View>
-                <Text style={{color: 'white'}}>1</Text>
-            </View>
         </MainWrapper>
     );
 }
